c7a43d264c3b6e072e423d9d06b0ef7fa9282555
Manish Nayak <nayak10softworld@gmail.com>
2024-08-23 16:30:00 +0530

    creating authentication for authorization with spring security

--- src/main/java/com/shop/ecomm/config/AppConfig.java
+++ src/main/java/com/shop/ecomm/config/AppConfig.java
@@ -7,7 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -18,11 +21,12 @@ public class AppConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		// Set session management to stateless
+		// Set session management to state less
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.authorizeHttpRequests(
 						Authorize -> Authorize.requestMatchers("/api/**").authenticated().anyRequest().permitAll())
-				.addFilterBefore(null, null).csrf().disable().cors().configurationSource(new CorsConfigurationSource() {
+				.addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class).csrf().disable().cors()
+				.configurationSource(new CorsConfigurationSource() {
 
 					@Override
 					public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
@@ -42,5 +46,11 @@ public class AppConfig {
 				}).and().httpBasic().and().formLogin();
 
 		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+
+		return new BCryptPasswordEncoder();
 	}
 }
---
+++ src/main/java/com/shop/ecomm/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.shop.ecomm.config;
+
+public class JwtConstant {
+
+	public static final String SECRET_KEY = "uiopretyghkjgligyruujdivhyfhfdvghvdubncioxcxifvkvj";
+
+	public static final String JWT_HEADER = "Authorization";
+
+}
