f7c44fff7d8fd6d146d873736800840d4dc841ce
Manish Nayak <nayak10softworld@gmail.com>
2024-08-23 12:26:10 +0530

    Creating new repository for back-end of Ecommerce Project with React + TailwindCSS + SpringBoot

---
+++ src/test/java/com/shop/ecomm/EcommerceProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.ecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
