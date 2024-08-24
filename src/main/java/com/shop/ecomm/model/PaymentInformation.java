0e907f1719eb41a5eac643948539ad97e4f5cd44
Manish Nayak <nayak10softworld@gmail.com>
2024-08-24 15:17:52 +0530

    paymentInformation cls has done , now working on rating cls

--- src/main/java/com/shop/ecomm/model/PaymentInformation.java
+++ src/main/java/com/shop/ecomm/model/PaymentInformation.java
@@ -1,5 +1,20 @@
 package com.shop.ecomm.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+
 public class PaymentInformation {
 
+	@Column(name = "cardHolder_name")
+	private String cardHolderName;
+
+	@Column(name = "card_number")
+	private String cardNumber;
+
+	@Column(name = "expiration_date")
+	private LocalDate expirationDate;
+
+	@Column(name = "cvv")
+	private String cvv;
 }
