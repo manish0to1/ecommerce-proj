037623315a8b12ad02d66121f7b7ebb18653a66d
Manish Nayak <nayak10softworld@gmail.com>
2024-08-25 18:15:57 +0530

    after rating cls now generating constructor , constructor with feilds and default constructor for user cls and getters & setters also

--- src/main/java/com/shop/ecomm/model/User.java
+++ src/main/java/com/shop/ecomm/model/User.java
@@ -59,4 +59,118 @@ public class User {
 		// TODO Auto-generated constructor stub
 	}
 
+	public User(Long id, String firstName, String lastName, String email, String password, String role, String mobile,
+			List<Address> address, List<PaymentInformation> paymentInformation, List<Rating> ratings,
+			List<Review> reviews, LocalDateTime createdAt) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.mobile = mobile;
+		this.address = address;
+		this.paymentInformation = paymentInformation;
+		this.ratings = ratings;
+		this.reviews = reviews;
+		this.createdAt = createdAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public List<Address> getAddress() {
+		return address;
+	}
+
+	public void setAddress(List<Address> address) {
+		this.address = address;
+	}
+
+	public List<PaymentInformation> getPaymentInformation() {
+		return paymentInformation;
+	}
+
+	public void setPaymentInformation(List<PaymentInformation> paymentInformation) {
+		this.paymentInformation = paymentInformation;
+	}
+
+	public List<Rating> getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(List<Rating> ratings) {
+		this.ratings = ratings;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
 }
