8ed59bb652a54f1dd32c7a76d06516d679ad0b26
Manish Nayak <nayak10softworld@gmail.com>
2024-08-25 17:20:18 +0530

    working on review cls

--- src/main/java/com/shop/ecomm/model/Review.java
+++ src/main/java/com/shop/ecomm/model/Review.java
@@ -1,5 +1,25 @@
 package com.shop.ecomm.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Review {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String review;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	@JsonIgnore
+	private Product product;
 }
