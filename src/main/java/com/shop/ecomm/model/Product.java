ab38a57236fedd28879ebf5bdc7dbc7e78a7fce3
Manish Nayak <nayak10softworld@gmail.com>
2024-08-25 18:03:20 +0530

    generating constructor , constructor with feilds and default constructor for Product cls and getters & setters also

--- src/main/java/com/shop/ecomm/model/Product.java
+++ src/main/java/com/shop/ecomm/model/Product.java
@@ -71,4 +71,154 @@ public class Product {
 		// TODO Auto-generated constructor stub
 	}
 
+	public Product(Long id, String title, String description, int price, int discountedPrice, int discountPersent,
+			int quantity, String brand, String color, Set<Size> size, String imageUrl, List<Rating> ratings,
+			List<Review> reviews, int numRating, Category category, LocalDateTime createdAt) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.price = price;
+		this.discountedPrice = discountedPrice;
+		this.discountPersent = discountPersent;
+		this.quantity = quantity;
+		this.brand = brand;
+		this.color = color;
+		this.size = size;
+		this.imageUrl = imageUrl;
+		this.ratings = ratings;
+		this.reviews = reviews;
+		this.numRating = numRating;
+		this.category = category;
+		this.createdAt = createdAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getDiscountedPrice() {
+		return discountedPrice;
+	}
+
+	public void setDiscountedPrice(int discountedPrice) {
+		this.discountedPrice = discountedPrice;
+	}
+
+	public int getDiscountPersent() {
+		return discountPersent;
+	}
+
+	public void setDiscountPersent(int discountPersent) {
+		this.discountPersent = discountPersent;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public Set<Size> getSize() {
+		return size;
+	}
+
+	public void setSize(Set<Size> size) {
+		this.size = size;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public List<Rating> getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(List<Rating> ratings) {
+		this.ratings = ratings;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	public int getNumRating() {
+		return numRating;
+	}
+
+	public void setNumRating(int numRating) {
+		this.numRating = numRating;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
 }
