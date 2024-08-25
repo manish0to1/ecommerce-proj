fed1f6991c968994167034891bbeed771e07ee14
Manish Nayak <nayak10softworld@gmail.com>
2024-08-25 17:57:53 +0530

    review cls has been done

--- src/main/java/com/shop/ecomm/model/Review.java
+++ src/main/java/com/shop/ecomm/model/Review.java
@@ -1,5 +1,7 @@
 package com.shop.ecomm.model;
 
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
@@ -22,4 +24,64 @@ public class Review {
 	@JoinColumn(name = "product_id")
 	@JsonIgnore
 	private Product product;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	private LocalDateTime createdAt;
+
+	public Review() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Review(Long id, String review, Product product, User user, LocalDateTime createdAt) {
+		super();
+		this.id = id;
+		this.review = review;
+		this.product = product;
+		this.user = user;
+		this.createdAt = createdAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getReview() {
+		return review;
+	}
+
+	public void setReview(String review) {
+		this.review = review;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
 }
