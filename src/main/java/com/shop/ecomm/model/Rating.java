bfd9a3d2b4378a26b9b0ea70472472fbf54fb728
Manish Nayak <nayak10softworld@gmail.com>
2024-08-25 18:12:00 +0530

    after Product cls now generating constructor , constructor with feilds and default constructor for Rating cls and getters & setters also

--- src/main/java/com/shop/ecomm/model/Rating.java
+++ src/main/java/com/shop/ecomm/model/Rating.java
@@ -37,4 +37,53 @@ public class Rating {
 		// TODO Auto-generated constructor stub
 	}
 
+	public Rating(Long id, User user, Product product, double rating, LocalDateTime creatAt) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.product = product;
+		this.rating = rating;
+		this.creatAt = creatAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	public LocalDateTime getCreatAt() {
+		return creatAt;
+	}
+
+	public void setCreatAt(LocalDateTime creatAt) {
+		this.creatAt = creatAt;
+	}
+
 }
