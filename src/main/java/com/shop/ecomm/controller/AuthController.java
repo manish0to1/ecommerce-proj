5d2bcfa392192b8cf2b82021fa2586fb9b2cadce
Manish Nayak <nayak10softworld@gmail.com>
2024-08-26 15:01:19 +0530

    working on authController and getting details from user

--- src/main/java/com/shop/ecomm/controller/AuthController.java
+++ src/main/java/com/shop/ecomm/controller/AuthController.java
@@ -1,10 +1,23 @@
 package com.shop.ecomm.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shop.ecomm.exception.UserException;
+import com.shop.ecomm.model.User;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+	public ResponseEntity<AuthResponse> createUserHandler(@ResponseBody User user) throws UserException {
+
+		String email = user.getEmail();
+		String password = user.getPassword();
+		String firstName = user.getFirstName();
+		String lastName = user.getLastName();
+
+	}
 }
