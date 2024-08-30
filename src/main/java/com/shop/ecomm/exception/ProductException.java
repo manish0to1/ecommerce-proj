65c35bdefd258d94b564b0e61c0e1101c03ecc3a
Manish Nayak <nayak10softworld@gmail.com>
2024-08-30 14:34:28 +0530

    creating ProductService and working on it with crud operations

---
+++ src/main/java/com/shop/ecomm/exception/ProductException.java
@@ -0,0 +1,8 @@
+package com.shop.ecomm.exception;
+
+public class ProductException extends Exception {
+
+	public ProductException(String message) {
+		super(message);
+	}
+}
--- src/main/java/com/shop/ecomm/service/ProductService.java
+++ src/main/java/com/shop/ecomm/service/ProductService.java
@@ -1,5 +1,24 @@
 package com.shop.ecomm.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.shop.ecomm.exception.ProductException;
+import com.shop.ecomm.model.Product;
+import com.shop.ecomm.request.CreateProductRequest;
+
 public interface ProductService {
 
+	public Product createProduct(CreateProductRequest req);
+
+	public Product deleteProduct(Long productId) throws ProductException;
+
+	public Product updateproduct(Long productId, Product product) throws ProductException;
+
+	public Product findProductById(Long Id) throws ProductException;
+
+	public List<Product> findProductByCategory(String category);
+
+	public Page<Product> getAllProductByCategory(String category);
 }
