baad53dc4e36d6bc2744ea3acdb96f5f6369b54a
Manish Nayak <nayak10softworld@gmail.com>
2024-09-01 16:46:08 +0530

    creating PaymentDetails and working on it

--- src/main/java/com/shop/ecomm/model/OrderItem.java
+++ src/main/java/com/shop/ecomm/model/OrderItem.java
@@ -31,4 +31,94 @@ public class OrderItem {
 	private Long userId;
 	private LocalDateTime deliveryDate;
 
+	public OrderItem() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public OrderItem(Long id, Order order, Product product, String size, int quantity, Integer price,
+			Integer discountedPrice, Long userId, LocalDateTime deliveryDate) {
+		super();
+		this.id = id;
+		this.order = order;
+		this.product = product;
+		this.size = size;
+		this.quantity = quantity;
+		this.price = price;
+		this.discountedPrice = discountedPrice;
+		this.userId = userId;
+		this.deliveryDate = deliveryDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Integer getDiscountedPrice() {
+		return discountedPrice;
+	}
+
+	public void setDiscountedPrice(Integer discountedPrice) {
+		this.discountedPrice = discountedPrice;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public LocalDateTime getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(LocalDateTime deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
 }
