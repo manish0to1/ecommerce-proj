f1a807b212782b11bd7b2125480d6a58d91592fe
Manish Nayak <nayak10softworld@gmail.com>
2024-09-01 19:26:54 +0530

    now creating all methods in orderService Implematation from OrderService

--- src/main/java/com/shop/ecomm/service/OrderServiceImpl.java
+++ src/main/java/com/shop/ecomm/service/OrderServiceImpl.java
@@ -1,5 +1,76 @@
 package com.shop.ecomm.service;
 
-public class OrderServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.shop.ecomm.exception.OrderException;
+import com.shop.ecomm.model.Address;
+import com.shop.ecomm.model.Order;
+import com.shop.ecomm.model.User;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+	// #################### Methods ###################### //
+	@Override
+	public Order createOrder(User user, Address shippingAddress) {
+		// Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order findOrderById(Long orderId) throws OrderException {
+		// Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> userOrderHistory(Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order placedOrder(Long orderId) throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order shippingOrder(Long orderId) throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order confirmedOrder(Long orderId) throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order deliveredOrder(Long orderId) throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order calceledOrder(Long orderId) throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> getAllOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteOrder(Long orderId) throws OrderException {
+		// TODO Auto-generated method stub
+
+	}
 
 }
