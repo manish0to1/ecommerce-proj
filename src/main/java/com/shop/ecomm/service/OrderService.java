a584a02e3dbcbec1b9e819d6ac97f823aa670888
Manish Nayak <nayak10softworld@gmail.com>
2024-09-01 17:14:07 +0530

    working on OrderService cls

--- src/main/java/com/shop/ecomm/service/OrderService.java
+++ src/main/java/com/shop/ecomm/service/OrderService.java
@@ -1,5 +1,24 @@
 package com.shop.ecomm.service;
 
+import java.util.List;
+
+import com.shop.ecomm.model.Order;
+import com.shop.ecomm.model.User;
+
 public interface OrderService {
+
+	public Order createOrder(User user, Address shippingAddress);
+
+	public Order findOrderById(Long orderId) throws OrderException;
+
+	public List<Order> userOrderHistory(Long userId);
+
+	public Order placedOrder(Long orderId) throws OrderException;
+
+	public Order confirmedOrder(Long orderId) throws OrderException;
+
+	public Order deliveredOrder(Long orderId) throws OrderException;
+
+	public Order calceledOrder(Long orderId) throws OrderException;
 
 }
