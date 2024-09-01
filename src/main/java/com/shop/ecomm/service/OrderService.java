4d32a37ac5c8ffa28054dee31b6e5b9e4d05fa03
Manish Nayak <nayak10softworld@gmail.com>
2024-09-01 19:21:28 +0530

    creating orderServiceImpl and working on it

--- src/main/java/com/shop/ecomm/service/OrderService.java
+++ src/main/java/com/shop/ecomm/service/OrderService.java
@@ -2,6 +2,8 @@ package com.shop.ecomm.service;
 
 import java.util.List;
 
+import com.shop.ecomm.exception.OrderException;
+import com.shop.ecomm.model.Address;
 import com.shop.ecomm.model.Order;
 import com.shop.ecomm.model.User;
 
@@ -15,10 +17,16 @@ public interface OrderService {
 
 	public Order placedOrder(Long orderId) throws OrderException;
 
+	public Order shippingOrder(Long orderId) throws OrderException;
+
 	public Order confirmedOrder(Long orderId) throws OrderException;
 
 	public Order deliveredOrder(Long orderId) throws OrderException;
 
 	public Order calceledOrder(Long orderId) throws OrderException;
+
+	public List<Order> getAllOrder();
+
+	public void deleteOrder(Long orderId) throws OrderException;
 
 }
