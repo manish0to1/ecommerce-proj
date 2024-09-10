5746ed459c9260e4c70219f7b4788967e7b4d77b
Manish Nayak <nayak10softworld@gmail.com>
2024-09-10 23:11:13 +0530

    working and implementing orderServiceImpl and also completing all neccessory methods

--- src/main/java/com/shop/ecomm/service/OrderServiceImpl.java
+++ src/main/java/com/shop/ecomm/service/OrderServiceImpl.java
@@ -1,34 +1,102 @@
 package com.shop.ecomm.service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.shop.ecomm.exception.OrderException;
 import com.shop.ecomm.model.Address;
+import com.shop.ecomm.model.Cart;
+import com.shop.ecomm.model.CartItem;
 import com.shop.ecomm.model.Order;
+import com.shop.ecomm.model.OrderItem;
 import com.shop.ecomm.model.User;
-import com.shop.ecomm.repository.CartRepository;
+import com.shop.ecomm.repository.UserRepository;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
-	private CartRepository cartRepository;
-	private CartService cartItemService;
-	private ProductService productService;
+//	private CartRepository cartRepository;
+//	private CartService cartItemService;
+//	private ProductService productService;
+//
+//	public OrderServiceImpl(CartRepository cartRepository, CartService cartItemService,
+//			ProductService productService) {
+//		this.cartRepository = cartRepository;
+//		this.cartItemService = cartItemService;
+//		this.productService = productService;
+//	}
 
-	public OrderServiceImpl(CartRepository cartRepository, CartService cartItemService,
-			ProductService productService) {
-		this.cartRepository = cartRepository;
-		this.cartItemService = cartItemService;
-		this.productService = productService;
+	private OrderRepository orderRepository;
+	private CartService cartService;
+	private AddressRepository addressRepository;
+	private UserRepository userRepository;
+	private OrderItemService orderItemService;
+	private OrderItemRepository orderItemRepository;
+
+	public OrderServiceImpl(OrderRepository orderRepository, CartService cartService,
+			AddressRepository addressRepository, UserRepository userRepository, OrderItemService orderItemService,
+			OrderItemRepository orderItemRepository) {
+
+		this.orderRepository = orderRepository;
+		this.cartService = cartService;
+		this.addressRepository = addressRepository;
+		this.userRepository = userRepository;
+		this.orderItemService = orderItemService;
+		this.orderItemRepository = orderItemRepository;
+
 	}
 
 	// #################### Methods ###################### //
 	@Override
 	public Order createOrder(User user, Address shippingAddress) {
-		// Auto-generated method stub
-		return null;
+
+		shippingAddress.setUser(user);
+		Address.address = addressRepository.save(shippAddress);
+		user.getAddresses().add(address);
+		userRepository.save(user);
+
+		Cart cart = cartService.findUserCart(user.getId());
+		List<OrderItem> orderItems = new ArrayList<>();
+
+		for (CartItem item : cart.getCartItems()) {
+			OrderItem orderItem = new OrderItem();
+
+			orderItem.setPrice(item.getPrice());
+			orderItem.setProduct(item.getProduct());
+			orderItem.setQuantity(item.getQuantity());
+			orderItem.setSize(item.getSize());
+			orderItem.setUserId(item.getUserId());
+			orderItem.setDiscountedPrice(item.getDiscountedPrice());
+
+			OrderItem createdOrderItem = orderItemRepository.save(orderItem);
+			orderItems.add(createdOrderItem);
+		}
+
+		Order createdOrder = new Order();
+
+		createdOrder.setUser(user);
+		createdOrder.setOrderItems(orderItems);
+		createdOrder.setTotalPrice(cart.getTotalPrice());
+		createdOrder.setToalDiscountedPrice(cart.getTotalDiscountedPrice());
+		createdOrder.setDiscount(cart.getDiscount());
+		createdOrder.setTotalItem(cart.getTotalItem());
+
+		createdOrder.setShippingAddress(address);
+		createdOrder.setOrderDate(LocalDateTime.now());
+		createdOrder.setOrderStatus(OrderStatus.PENDING);
+		createdOrder.setPaymentDetails().setStatus(PaymentStatus.PENDING);
+		createdOrder.setCreateAt(LocalDateTime.now());
+
+		Order savedOrder = orderItemRepository.save(createdOrder);
+
+		for (OrderItem item : orderItems) {
+			item.setOrder(savedOrder);
+			orderRepository.save(item);
+		}
+		return savedOrder;
 	}
 
 	@Override
