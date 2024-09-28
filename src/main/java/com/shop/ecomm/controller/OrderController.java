f39fc88b61842b8f6f3a4a419fe0ee03947db0a2
Manish Nayak <nayak10softworld@gmail.com>
2024-09-28 19:50:10 +0530

    orderController has done

--- src/main/java/com/shop/ecomm/controller/OrderController.java
+++ src/main/java/com/shop/ecomm/controller/OrderController.java
@@ -1,5 +1,64 @@
 package com.shop.ecomm.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shop.ecomm.exception.OrderException;
+import com.shop.ecomm.exception.UserException;
+import com.shop.ecomm.model.Address;
+import com.shop.ecomm.model.Order;
+import com.shop.ecomm.model.User;
+import com.shop.ecomm.service.OrderService;
+import com.shop.ecomm.service.UserService;
+
+@RestController
+@RequestMapping("/api/orders")
 public class OrderController {
 
+	@Autowired
+	private OrderService orderService;
+
+	@Autowired
+	private UserService userService;
+
+	@PostMapping("/")
+	public ResponseEntity<Order> createOrder(@RequestBody Address shippingAdress,
+			@RequestHeader("Authorization") String jwt) throws UserException {
+		User user = userService.findUserProfileByJwt(jwt);
+
+		Order order = orderService.createOrder(user, shippingAdress);
+
+		System.out.println("order" + order);
+
+		return new ResponseEntity<Order>(order, HttpStatus.CREATED);
+
+	}
+
+	@GetMapping("/user")
+	public ResponseEntity<List<Order>> usersOrderHistory(@RequestHeader("Authorization") String jwt)
+			throws UserException {
+		User user = userService.findUserProfileByJwt(jwt);
+
+		List<Order> orders = orderService.userOrderHistory(user.getId());
+		return new ResponseEntity<>(orders, HttpStatus.CREATED);
+	}
+
+	public ResponseEntity<Order> findOrderById(@PathVariable("id") Long orderid,
+			@RequestHeader("Authorization") String jwt) throws UserException, OrderException {
+
+		User user = userService.findUserProfileByJwt(jwt);
+		Order order = orderService.findOrderById(orderid);
+
+		return new ResponseEntity<>(order, HttpStatus.CREATED);
+	}
 }
