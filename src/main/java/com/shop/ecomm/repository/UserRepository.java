7dd1580666026c6e4cc21e5e94b09baac5324eb9
Manish Nayak <nayak10softworld@gmail.com>
2024-10-02 14:53:12 +0530

    error resolved yet , & i m waiting for next !

--- src/main/java/com/shop/ecomm/repository/UserRepository.java
+++ src/main/java/com/shop/ecomm/repository/UserRepository.java
@@ -1,15 +1,38 @@
+
 package com.shop.ecomm.repository;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.shop.ecomm.model.Order;
 import com.shop.ecomm.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	// Find a user by email
 	public User findByEmail(String email);
 
-	public List<Order> getUsersOrders(Long userId);
+	// Custom query to retrieve orders associated with a user
+	@Query("SELECT o FROM Order o WHERE o.user.id = :userId")
+	public List<Order> findOrdersByUserId(@Param("userId") Long userId);
 }
+
+// ####################### Original #################### //
+//package com.shop.ecomm.repository;
+//
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.shop.ecomm.model.Order;
+//import com.shop.ecomm.model.User;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//
+//	public User findByEmail(String email);
+//
+//	public List<Order> getUsersOrders(Long userId);
+//}
--- src/main/java/com/shop/ecomm/service/OrderServiceImpl.java
+++ src/main/java/com/shop/ecomm/service/OrderServiceImpl.java
@@ -106,7 +106,7 @@ public class OrderServiceImpl implements OrderService {
 
 	@Override
 	public List<Order> userOrderHistory(Long userId) {
-		List<Order> orders = userRepository.getUsersOrders(userId);
+		List<Order> orders = userRepository.findOrdersByUserId(userId);
 		return orders;
 	}
 
